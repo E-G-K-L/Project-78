883cce7e363c10078cb134a7e76873d1feab5b2d
E-G-K-L <88703447+E-G-K-L@users.noreply.github.com>
2022-04-30 22:42:40 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg","https://i.postimg.cc/qqyYvVbq/grandpa.jpg", "https://i.postimg.cc/wjMnFtMX/father.jpg" , "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg", "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
-var names = ["Fmaily Book","Ranbir Singh", "Diljeet Singh", "Rocky Singh", "Alia Singh", "Soni Singh"];
+var images = ["Family.jpg" ,  "irfan.jpg", "FILIZ.jpg", "Ezgi2.jpg", "DENIZ.jpg", "OZAN.jpg", "Deniz+Ezgii.JPG", "sibilings2.jpg"];
+var names = ["Family","Irfan Kula", "Filiz Kula", "Ezgi Kula", "Deniz Kula", "Ozan Kula", "Deniz and Ezgi", "Ezgi, Deniz, and Ozan"];
 var i = 0;
 function update()
 {
     i++;
-    var numbers_of_family_member_in_array = 5
+    var numbers_of_family_member_in_array = 8
     if(i > numbers_of_family_member_in_array)
       {
           i = 0;
